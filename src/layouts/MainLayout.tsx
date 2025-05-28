91d1698d4bf496958bc321b8ab1ee34a45a9941a
Madhusanka Sandaruwan Karunarathna <72614637+Madhusankamsk@users.noreply.github.com>
2025-05-28 22:21:31 +0530

    styles

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
-import { useAuth } from '../contexts/AuthContext';
 
 const MainLayout: React.FC = () => {
-  const { isAuthenticated } = useAuth();
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <Header />
-      <main className={`container mx-auto flex-grow ${isAuthenticated ? 'pt-20' : 'pt-16'} pb-8 sm:pb-12 md:pb-16`}>
+      <main className={`container mx-auto flex-grow pb-8 sm:pb-12 md:pb-16`}>
         <Outlet />
       </main>
       <Footer />
